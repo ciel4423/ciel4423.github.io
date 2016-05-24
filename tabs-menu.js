d1666c62f5356de267b3f7ed4a2739b07fa73629
ciel4423 <alicedive4423@gmail.com>
2016-05-24 22:48:07 +0900

    修正02

--- tabs-menu.js
+++ tabs-menu.js
@@ -1,9 +1,93 @@
 $(function() {
-    $("ul[class^=tabmenu] li").click(function() {
-        var num = $("ul[class^=tabmenu] li").index(this);
-        $("div[class^=tabContent]").removeClass('active');
-        $("div[class^=tabContent]").eq(num).addClass('active');
-        $("ul[class^=tabmenu] li").removeClass('active');
+    $(".tabmenu1 li").click(function() {
+        var num = $(".tabmenu1 li").index(this);
+        $(".tabContent1").removeClass('active');
+        $(".tabContent1").eq(num).addClass('active');
+        $(".tabmenu1 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+$(function() {
+    $(".tabmenu2 li").click(function() {
+        var num = $(".tabmenu2 li").index(this);
+        $(".tabContent2").removeClass('active');
+        $(".tabContent2").eq(num).addClass('active');
+        $(".tabmenu2 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+$(function() {
+    $(".tabmenu3 li").click(function() {
+        var num = $(".tabmenu3 li").index(this);
+        $(".tabContent3").removeClass('active');
+        $(".tabContent3").eq(num).addClass('active');
+        $(".tabmenu3 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+$(function() {
+    $(".tabmenu4 li").click(function() {
+        var num = $(".tabmenu4 li").index(this);
+        $(".tabContent4").removeClass('active');
+        $(".tabContent4").eq(num).addClass('active');
+        $(".tabmenu4 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+$(function() {
+    $(".tabmenu5 li").click(function() {
+        var num = $(".tabmenu5 li").index(this);
+        $(".tabContent5").removeClass('active');
+        $(".tabContent5").eq(num).addClass('active');
+        $(".tabmenu5 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+$(function() {
+    $(".tabmenu6 li").click(function() {
+        var num = $(".tabmenu6 li").index(this);
+        $(".tabContent6").removeClass('active');
+        $(".tabContent6").eq(num).addClass('active');
+        $(".tabmenu6 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+$(function() {
+    $(".tabmenu7 li").click(function() {
+        var num = $(".tabmenu7 li").index(this);
+        $(".tabContent7").removeClass('active');
+        $(".tabContent7").eq(num).addClass('active');
+        $(".tabmenu7 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+
+$(function() {
+    $(".tabmenu8 li").click(function() {
+        var num = $(".tabmenu8 li").index(this);
+        $(".tabContent8").removeClass('active');
+        $(".tabContent8").eq(num).addClass('active');
+        $(".tabmenu8 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+
+$(function() {
+    $(".tabmenu9 li").click(function() {
+        var num = $(".tabmenu9 li").index(this);
+        $(".tabContent9").removeClass('active');
+        $(".tabContent9").eq(num).addClass('active');
+        $(".tabmenu9 li").removeClass('active');
+        $(this).addClass('active')
+    });
+});
+
+$(function() {
+    $(".tabmenu10 li").click(function() {
+        var num = $(".tabmenu10 li").index(this);
+        $(".tabContent10").removeClass('active');
+        $(".tabContent10").eq(num).addClass('active');
+        $(".tabmenu10 li").removeClass('active');
         $(this).addClass('active')
     });
 });
