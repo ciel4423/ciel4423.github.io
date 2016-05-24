1b25a95dfc50551cd601c29901e28e79e20f9fe5
ciel4423 <alicedive4423@gmail.com>
2016-05-24 22:46:23 +0900

    修正01

--- tabs-menu.js
+++ tabs-menu.js
@@ -1,9 +1,9 @@
 $(function() {
-    $("[class^=tabmenu] li").click(function() {
-        var num = $("[class^=tabmenu] li").index(this);
-        $("[class^=tabContent]").removeClass('active');
-        $("[class^=tabContent]").eq(num).addClass('active');
-        $("[class^=tabmenu] li").removeClass('active');
+    $("ul[class^=tabmenu] li").click(function() {
+        var num = $("ul[class^=tabmenu] li").index(this);
+        $("div[class^=tabContent]").removeClass('active');
+        $("div[class^=tabContent]").eq(num).addClass('active');
+        $("ul[class^=tabmenu] li").removeClass('active');
         $(this).addClass('active')
     });
 });
