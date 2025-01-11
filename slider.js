ca43cf6073990c3be05721186adeff4001058ce4
ciel4423 <alicedive4423@gmail.com>
2025-01-11 16:20:47 +0900

    ver1.1

--- slider.js
+++ slider.js
@@ -1,16 +1,7 @@
-let slideIndex = [1, 1];
+let slideIndex = Array(slideId.length).fill(1);
 let slideId = ["slideshow1", "slideshow2", "slideshow3", "slideshow4", "slideshow5", "slideshow6", "slideshow7", "slideshow8", "slideshow9", "slideshow10"];
-showSlides(1, 0);
-showSlides(1, 1);
-showSlides(1, 2);
-showSlides(1, 3);
-showSlides(1, 4);
-showSlides(1, 5);
-showSlides(1, 6);
-showSlides(1, 7);
-showSlides(1, 8);
-showSlides(1, 9);
-showSlides(1, 10);
+
+slideId.forEach((id, index) => showSlides(1, index));
 
 function plusSlides(n, no) {
     showSlides(slideIndex[no] += n, no);
@@ -23,13 +14,13 @@ function currentSlide(n, no) {
 function showSlides(n, no) {
     let i;
     const slides = document.getElementById(slideId[no]).getElementsByClassName("slides");
-    const pageContainer = document.getElementById(`page-container${no+1}`);
-    if (n > slides.length) { slideIndex[no] = 1 }
-    if (n < 1) { slideIndex[no] = slides.length }
+    const pageContainer = document.getElementById(`page-container${no + 1}`);
+    if (n > slides.length) { slideIndex[no] = 1; }
+    if (n < 1) { slideIndex[no] = slides.length; }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
-    slides[slideIndex[no]-1].style.display = "block";
+    slides[slideIndex[no] - 1].style.display = "block";
 
     // ページ番号の更新
     pageContainer.innerHTML = "";
